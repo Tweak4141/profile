58faa5759f152b5d5b5c8c6687fb8ce701db6187
Tweak <tweak@daftscientist.com>
2022-01-11 18:54:39 -0700

    Update Database.kt

--- src/main/kotlin/data/Database.kt
+++ src/main/kotlin/data/Database.kt
@@ -9,8 +9,12 @@ import kotlin.concurrent.thread
 
 fun createHikariDataSource(): DataSource {
     val databaseUrl = System.getenv("JDBC_DATABASE_URL") ?: error("DATABASE_URL environment variable is not set.")
+    val databasePass = System.getenv("DATABASE_PASS") ?: error("DATABASE_PASS environment variable is not set.")
+    val databaseUser = System.getenv("DATABASE_USER") ?: error("DATABASE_USER environment variable is not set.")
     val config = HikariConfig().apply {
         jdbcUrl = databaseUrl
+        username = databaseUser
+        password = databasePass
     }
     val ds = HikariDataSource(config)
 
