6be7b6e58c2115e32ca9c1c14594d76ed1c65ec1
Tweak <tweak@daftscientist.com>
2022-01-13 21:03:11 -0700

    Create Eval.kt

---
+++ src/main/kotlin/bot/commands/Eval.kt
@@ -0,0 +1,52 @@
+package bot.commands
+
+import data.repository.BannerPreferenceRepository
+import discord4j.core.event.domain.interaction.SlashCommandEvent
+import discord4j.discordjson.json.ApplicationCommandOptionData
+import discord4j.discordjson.json.ApplicationCommandRequest
+import discord4j.rest.util.ApplicationCommandOptionType
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.http.*
+import kotlinx.coroutines.reactor.awaitSingle
+import org.koin.mp.KoinPlatformTools
+import utils.getOrNull
+import java.nio.channels.UnresolvedAddressException
+
+
+/**
+ * Command to eval Nashorn code.
+ */
+object Eval : Command {
+    override val name: String = "eval"
+
+    override fun commandRequest(): ApplicationCommandRequest =
+        ApplicationCommandRequest.builder()
+            .name(name)
+            .description("Evaluate Nashorn code.")
+            .addOption(ApplicationCommandOptionData.builder()
+                .name("code")
+                .description("Code to evaluate.")
+                .type(ApplicationCommandOptionType.STRING.value)
+                .required(false)
+                .build())
+            .build()
+
+    override suspend fun execute(event: SlashCommandEvent) {
+        val userId = event.interaction.user.id.asLong()
+        val ownerId = 440130952769830912
+        val code_to_exec = event.getOption("code").getOrNull()?.value?.getOrNull()?.asString()
+        if (userId != ownerId) return event.replyEphemeral("You don't have access to this commmand.").awaitSingle()
+        // Validate if code exists
+        if (code_to_exec != null) {
+            try {
+                event.replyEphemeral(" Evaluated Successfully:\n```\n"+se.eval(code_to_exec)+" ```").awaitSingle()
+                } catch(Exception e) {
+                event.replyEphemeral(" An exception was thrown:\n```\n"+e+" ```").awaitSingle()
+                }
+        } else {
+            event.replyEphemeral("No code was provided, cancelling.").awaitSingle()
+        }
+    }
+}
